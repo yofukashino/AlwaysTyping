c10fb3edc92ae16738dd8cbcae21186df35439ed
Tharki-god <alokender018@gmail.com>
2023-03-12 05:29:41 +0530

    better handling for custom channelIds

--- src/patches/UserTypingStore.ts
+++ src/patches/UserTypingStore.ts
@@ -3,13 +3,16 @@ import { SelectedChannelStore, UserTypingStore } from "../lib/requiredModules";
 import { defaultSettings } from "../lib/consts";
 import * as Utils from "../lib/utils";
 export const patchUserTypingStore = (): void => {
-  PluginInjector.after(
-    UserTypingStore,
-    "isTyping",
-    ([channelId, userId]: [string, string], res: boolean) => {
-      const CurrentChannelId = SelectedChannelStore.getCurrentlySelectedChannelId();
-      if (CurrentUser.id === userId && CurrentChannelId === channelId) Utils.type();
-      return res || SettingValues.get("showAllTyping", defaultSettings.showAllTyping);
-    },
-  );
+  PluginInjector.after(UserTypingStore, "isTyping", (_args: [string, string], res: boolean) => {
+    const channelIds = [
+      ...SettingValues.get("customChannels", defaultSettings.customChannels).split(","),
+      SelectedChannelStore.getCurrentlySelectedChannelId(),
+    ].filter((n) => n);
+    const shouldType = !channelIds.some((channelId) => {
+      const typingUsers = Object.keys(UserTypingStore.getTypingUsers(channelId));
+      return typingUsers.includes(CurrentUser.id);
+    });
+    if (shouldType) Utils.type();
+    return res || SettingValues.get("showAllTyping", defaultSettings.showAllTyping);
+  });
 };
