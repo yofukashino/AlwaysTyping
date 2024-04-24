8faca834c59e3cd025620b65b3a939e44bcf3a73
Tharki-God <alokender018@gmail.com>
2024-04-24 13:22:09 +0530

    pretty baby

--- src/injections/TypingStore.ts
+++ src/injections/TypingStore.ts
@@ -5,8 +5,12 @@ import { defaultSettings } from "../lib/consts";
 import Utils from "../lib/utils";
 
 export default (): void => {
-  PluginInjector.after(Modules.TypingStore, "isTyping", ([, userId]: [string, string], res: boolean) => {
-    if (!res && userId === UltimateUserStore.getCurrentUser()?.id) Utils.type();
-    return res || SettingValues.get("showAllTyping", defaultSettings.showAllTyping);
-  });
+  PluginInjector.after(
+    Modules.TypingStore,
+    "isTyping",
+    ([, userId]: [string, string], res: boolean) => {
+      if (!res && userId === UltimateUserStore.getCurrentUser()?.id) Utils.type();
+      return res || SettingValues.get("showAllTyping", defaultSettings.showAllTyping);
+    },
+  );
 };
