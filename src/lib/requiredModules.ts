ad73fa6f409b8c5a6a1461290fe67deb889a2b3a
Tharki-God <alokender018@gmail.com>
2024-06-26 03:31:45 +0530

    swc

--- src/lib/requiredModules.ts
+++ src/lib/requiredModules.ts
@@ -4,10 +4,14 @@ import Types from "../types";
 const Modules: Types.Modules = {};
 
 Modules.loadModules = async () => {
-  Modules.TypingUtils ??= await webpack.waitForProps<Types.TypingUtils>(
-    "startTyping",
-    "stopTyping",
-  );
+  Modules.TypingUtils ??= await webpack
+    .waitForProps<Types.TypingUtils>(["startTyping", "stopTyping"], {
+      timeout: 10000,
+    })
+    .catch(() => {
+      throw new Error("Failed To Find TypingUtils Module");
+    });
+
   Modules.SelectedChannelStore =
     webpack.getByStoreName<Types.SelectedChannelStore>("SelectedChannelStore");
   Modules.TypingStore = webpack.getByStoreName<Types.TypingStore>("TypingStore");
